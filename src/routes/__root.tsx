175b716dbc9d27dd5948069cd3e7922bd85c355c
harshithrao25-creator <harshithrao25@gmail.com>
2026-07-19 21:02:41 +0400

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -82,7 +82,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-     { rel: "icon", type: "image/png", href: logo },
+    {
+  rel: "icon",
+  type: "image/png",
+  href: "/favicon.png",
+},
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Sora:wght@500;600;700;800&family=Inter:wght@400;500;600;700&display=swap" },
